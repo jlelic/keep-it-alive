a9a0d2e5fa7a9f66b9e4d20834c955c0b077a9a2
Allinius <michal.durovec@gmail.com>
2020-04-18 20:18:50 +0200

    item destruction, wind, shake prototype

--- Assets/Scripts/Inventory/ItemManager.cs
+++ Assets/Scripts/Inventory/ItemManager.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class ItemManager : MonoBehaviour
 {
-    public float spawnInterval = 4.0f;
+    public float windForceX = 0.0f;
+    public float shakeStrength = 0.0f;
+    public float shakeInterval = 1.0f;
+    public GameObject inventoryContainer;
+    public bool testing = false; //TOREMOVE
+    public float spawnInterval = 4.0f; //TOREMOVE
     public GameObject[] items; //TOREMOVE
 
     float lastSpawn = 0;
@@ -12,10 +17,38 @@ public class ItemManager : MonoBehaviour
 
     void Update()
     {
-        if (Time.time - lastSpawn > spawnInterval) { //TOREMOVE
+        if (testing && Time.time - lastSpawn > spawnInterval) { //TOREMOVE
             lastSpawn = Time.time;
             SpawnItem(null);
         }
+
+        List<GameObject> itemsToDestroy = new List<GameObject>();
+        float despawnY = 2 * Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).y;
+        foreach(GameObject spawnedItem in spawnedItems)
+        {
+            if (spawnedItem == null || spawnedItem.transform.position.y < despawnY) {
+                itemsToDestroy.Add(spawnedItem);
+            }
+        }
+        foreach(GameObject itemToDestroy in itemsToDestroy)
+        {
+            spawnedItems.Remove(itemToDestroy);
+            Destroy(itemToDestroy);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        foreach(GameObject spawnedItem in spawnedItems)
+        {
+            if (spawnedItem != null && windForceX != 0)
+            {
+                Debug.Log("err");
+                Rigidbody2D itemRb2D = spawnedItem.GetComponent<Rigidbody2D>();
+                itemRb2D.AddForce(new Vector2(windForceX, 0));
+            }
+        }
+        
     }
 
     public GameObject SpawnItem(GameObject itemToSpawn)
@@ -25,11 +58,12 @@ public class ItemManager : MonoBehaviour
             spawnedItems.Add(item);
             return item;
         }
-        else if (items[0] != null) //TOREMOVE
+        else if (testing && items[0] != null) //TOREMOVE
         {
             GameObject item = Instantiate(items[0], transform.position, transform.rotation);
             InteractiveItem itemComponent = item.GetComponent<InteractiveItem>();
             itemComponent.id = "zmrzlina";
+            spawnedItems.Add(item);
             return item;
         }
         return null;
---
+++ Assets/Scripts/Inventory/Shake.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shake : MonoBehaviour
+{
+    public float shakeStrength = 1.0f;
+    public float shakeInterval = 1.0f;
+    
+    Vector3 originalPosition;
+    float lastDestinationChange = 0.0f;
+    Vector3 destination;
+
+    void Start() {
+        originalPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (Time.time - lastDestinationChange > shakeInterval)
+        {
+            lastDestinationChange = Time.time;
+            Vector3 offset = Random.insideUnitSphere * shakeStrength;
+            offset.z = transform.position.z;
+            Debug.Log(offset);
+            destination = originalPosition + offset;
+            Debug.Log(destination);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (destination != null && lastDestinationChange > 0)
+        {
+            Vector3 direction = destination - transform.position;
+            transform.position += (0.1f / shakeInterval) * direction;
+        }
+    }
+}
