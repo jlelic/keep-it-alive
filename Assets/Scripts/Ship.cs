67f96a0d33beeb006e42fd68c966c199156a84fa
Jozef Lelic <joseph.lelic@gmail.com>
2020-04-19 09:10:09 +0200

    more edo

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.rigidbody == null)
+        {
+            return;
+        }
+
+        if (collision.rigidbody.gameObject.GetComponent<EnemyCar>() != null)
+        {
+            GameManager.Instance.TakeHit();
+            GameManager.Instance.DirtyWindow();
+        }
+    }
+
+}
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using static iTween;
+using System;
+
+public enum ItemType
+{
+    TRASH,
+    ICECREAM
+}
+
+public class Utils
+{
+    public static Vector2 V3toV2(Vector3 vector)
+    {
+        return new Vector2(vector.x, vector.y);
+    }
+
+    public static void tweenColor(Graphic graphic, Color color, float time, float delay = 0, EaseType easeType = EaseType.linear, LoopType loopType = LoopType.none, bool ignoreTimeScale = false)
+    {
+        ValueTo(graphic.gameObject, Hash(
+         "from", graphic.color,
+         "to", color,
+         "delay", delay,
+         "time", time,
+         "looptype", loopType,
+         "easetype", easeType,
+         "ignoretimescale", ignoreTimeScale,
+         "onupdate", (Action<Color>)(newColor =>
+         {
+             graphic.color = newColor;
+         })
+        ));
+    }
+
+    public static string ToTimeString(float seconds)
+    {
+        int m = Mathf.FloorToInt(seconds / 60F);
+        int s = Mathf.FloorToInt(seconds - m * 60);
+        return string.Format("{0:0}:{1:00}", m, s);
+    }
+
+    public static void PlayAudio(AudioSource source, bool randomPitch = false)
+    {
+        source.pitch = randomPitch ? UnityEngine.Random.Range(0.75f, 1.5f) : 1;
+        source.Play();
+    }
+
+    public static void PlayAudio(AudioSource source, AudioClip clip, bool randomPitch = false)
+    {
+        source.pitch = randomPitch ? UnityEngine.Random.Range(0.75f, 1.5f) : 1;
+        source.clip = clip;
+        source.Play();
+    }
+}
